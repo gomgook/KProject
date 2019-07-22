22801c624e01ea73f09a14d7a2f36e580248a2bd
gomguk <kaioga4187@gmail.com>
2019-07-22 23:10:44 +0900

    feat: add back button into ContentActivity
    Fixes https://github.com/gomgook/KProject/issues/18

--- app/src/main/java/com/gomguk/kproject/content/ContentActivity.kt
+++ app/src/main/java/com/gomguk/kproject/content/ContentActivity.kt
@@ -2,10 +2,13 @@ package com.gomguk.kproject.content
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.gomguk.kproject.R
+import android.view.MenuItem
 import com.gomguk.kproject.util.model.Document
 import com.gomguk.kproject.util.view.loadImage
 import kotlinx.android.synthetic.main.activity_content.*
+import android.support.v4.app.NavUtils
+import com.gomguk.kproject.R
+
 
 class ContentActivity : AppCompatActivity(), ContentContract.View {
     override lateinit var presenter: ContentPresenter
@@ -13,6 +16,7 @@ class ContentActivity : AppCompatActivity(), ContentContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
 
         presenter = ContentPresenter(ContentRepository.getInstance(), this)
         val data = presenter.getData(intent)
@@ -25,6 +29,16 @@ class ContentActivity : AppCompatActivity(), ContentContract.View {
 
         data.thumbnail?.let {
             image.loadImage(it)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                NavUtils.navigateUpFromSameTask(this)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
